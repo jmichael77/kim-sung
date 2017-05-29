9da4cc5215fd23890f179c322bb4424cecbc9e16
jmichael77 <jmichaely2k7@gmail.com>
2017-05-29 17:10:08 +0100

    First Version Of kim-sung

--- kim-sung/ViewController.swift
+++ kim-sung/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    var leftScore = 0
+    
+    let cardName = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        //Randomize Left Number From 0 To 12
+        let leftNumber = Int(arc4random_uniform(13))
+        
+        //Set Left Image
+        leftImageView.image = UIImage(named: cardName[leftNumber])
+        
+        //Randomize Right Number From 0 To 12
+        let rightNumber = Int(arc4random_uniform(13))
+        
+        //Set Right Image
+        rightImageView.image = UIImage(named: cardName[rightNumber])
+        
+        //Compare The Card Numbers
+        if leftNumber > rightNumber {
+            //leftCard Wins
+            leftScore += 1
+            
+            //Update Label
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber == rightNumber {
+            //It's a tie
+        }
+        else {
+            //rightCard Wins
+            rightScore += 1
+            
+            //Update Label
+            rightScoreLabel.text = String(rightScore)
+        }
+    }
+    
+    //Instagram Button 
+    
+    @IBAction func fpsTapped(_ sender: Any) {
+    
+        let Username =  "1fps" // Your Instagram Username here
+        let appURL = NSURL(string: "instagram://user?username=\(Username)")!
+        let webURL = NSURL(string: "https://instagram.com/\(Username)")!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL as URL) {
+            application.open(appURL as URL)
+        } else {
+            // if Instagram app is not installed, open URL inside Safari
+            application.open(webURL as URL)
+        }
+        
+//    let url = URL(string: "instagram://user?username=1fps")
+//    UIApplication.shared.canOpenURL(url!)
+//    
+//    UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+        
+    }
 }
 
